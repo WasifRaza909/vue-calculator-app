9bfb742f254ed34916a315e49c73c8cc6741a7d0
Wasif Raza <wasifraza909@gmail.com>
2023-05-24 15:24:46 +0500

    Completed app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CalculatorComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculatorComponent from "./components/CalculatorComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CalculatorComponent,
+  },
+};
 </script>
 
 <style>
@@ -22,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  background: #31475e;
 }
 </style>
